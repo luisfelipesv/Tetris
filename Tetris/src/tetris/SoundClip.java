cde848e4c861d7d8cee75bb2e025b48dd7874840
Melissa Treviño <mely.trevic@gmail.com>
2016-03-02 14:28:49 -0600

    Standardization of SoundClip file and arrangements in .wavs
    Changed comments in SoundClip file to match standards, and trimmed the
    .wav files so there won’t be empty spaces in the beginning.

--- Tetris/src/tetris/SoundClip.java
+++ Tetris/src/tetris/SoundClip.java
@@ -1,7 +1,8 @@
 package tetris;
 
 /**
- * Clase SoundClip
+ * Class SoundClip
+ *
  * @melytc
  *
  */
@@ -15,173 +16,202 @@ import java.net.URL;
 
 public class SoundClip {
 
-	private AudioInputStream sample;
-	private Clip clip;
-	private boolean looping = false;
-	private int repeat = 0;
-	private String filename = "";
-        private long clipTime = 0;
+    private AudioInputStream aiSample;
+    private Clip cClip;
+    private boolean bLooping = false;
+    private int iRepeat = 0;
+    private String sFilename = "";
+    private long lClipTime = 0;
 
-	/**
-	 * Constructor default
-	 */
-	public SoundClip() {
-		try {
-			//crea el Buffer de sonido
-			clip = AudioSystem.getClip();
-		} catch (LineUnavailableException e) { 
-
-		}
-	}
-
-	/** 
-	 * Constructor con parametros, que carga manda llamar a load
-	 * esto carga el archivo de sonido.
-	 * @param filename es el <code>String</code> del archivo.
-	 */
-	public SoundClip(String filename) {
-		//Llama al constructor default.
-		this();
-		//Carga el archivo de sonido.
-		load(filename);
-	}
-
-	/** 
-	 * Metodo de acceso que regresa un objeto de tipo Clip
-	 * @return clip es un <code>objeto Clip</code>.
-	 */
-	public Clip getClip() { 
-		return clip; 
-	}
-
-	/** 
-	 * Metodo modificador usado para modificar si el sonido se repite.
-	 * @param _looping es un valor <code>boleano</code>. 
-	 */
-	public void setLooping(boolean looping) {
-		this.looping = looping; 
-	}
-
-	/** 
-	 * Metodo de acceso que regresa un booleano para ver si hay repeticion.
-	 * @return looping  es un valor <code>boleano</code>. 
-	 */
-	public boolean getLooping() {
-		return looping;
-	}
-
-	/** 
-	 * Metodo modificador usado para definir el numero de repeticiones.
-	 * @param _repeat es un <code>entero</code> que es el numero de repeticiones. 
-	 */
-	public void setRepeat(int repeat) {
-		this.repeat = repeat;
-	}
-
-	/** 
-	 * Metodo de acceso que regresa el numero de repeticiones.
-	 * @return repeat es un valor <code>entero</code> con el numero de repeticiones. 
-	 */
-	public int getRepeat() { 
-		return repeat; 
-	}
-
-	/**
-	 * Metodo modificador que asigna un nombre al archivo.
-	 * @param _filename es un <code>String</code> con el nombre del archivo. 
-	 */
-	public void setFilename(String filename) { 
-		this.filename = filename; 
-	}
-
-	/** 
-	 * Metodo de acceso que regresa el nombre del archivo.
-	 * @return filename es un <code>String</code> con el nombre del archivo. 
-	 */
-	public String getFilename() { 
-		return filename;
-	}
-
-	/**
-	 * Metodo que verifica si el archivo de audio esta cargado.
-	 * @return sample es un <code>objeto sample</code>.
-	 */
-	public boolean isLoaded() {
-		return (boolean)(sample != null);
-	}
-
-	/** 
-	 * Metodo de acceso que regresa el url del archivo
-	 * @param filename es un <code>String</code> con el nombre del archivo. 
-	 */
-	private URL getURL(String filename) {
-		URL url = null;
-		try {
-			url = this.getClass().getResource(filename);
-		}
-		catch (Exception e) { 
-
-		}
-		return url;
-	}
-
-	/** 
-	 * Metodo que carga el archivo de sonido.
-	 * @param audiofile es un <code>String</code> con el nombre del archivo de sonido.
-	 */
-	public boolean load(String audiofile) {
-		try {
-			setFilename(audiofile);
-			sample = AudioSystem.getAudioInputStream(getURL(filename));
-			clip.open(sample);
-			return true;
-
-		} catch (IOException e) {
-			return false;
-		} catch (UnsupportedAudioFileException e) {
-			return false;
-		} catch (LineUnavailableException e) {
-			return false;
-		}
-	}
-
-	/**
-	 * Metodo que reproduce el sonido.
-	 */
-	public void play() {
-		//se sale si el sonido no a sido cargado
-		if (!isLoaded()) 
-			return;
-		//vuelve a empezar el sound clip
-		clip.setFramePosition(0);
-
-		//Reproduce el sonido con repeticion opcional.
-		if (looping)
-			clip.loop(Clip.LOOP_CONTINUOUSLY);
-		else
-			clip.loop(repeat);
-	}
-
-	/**
-	 * Metodo que detiene el sonido.
-	 */
-	public void stop() {
-		clip.stop();
-                clipTime = 0;
-	}
-        
-        public void pause() {
-            clipTime= clip.getMicrosecondPosition();
-            clip.stop();
-
+    /**
+     * Default constructor of class SoundClip.
+     */
+    public SoundClip() {
+        try {
+            // Creates the Buffer with the sound.
+            cClip = AudioSystem.getClip();
+        } catch (LineUnavailableException excLine) {
         }
-        
-        public void unpause() {
-            clip.setMicrosecondPosition(clipTime);
-            //Reproduce el sonido con repeticion opcional.
-		if (looping)
-			clip.loop(Clip.LOOP_CONTINUOUSLY);
-		else
-			clip.loop(repeat);
+    }
+
+    /**
+     * Constructor with parameters. Calls 'load' function to load the sound
+     * file.
+     *
+     * @param sFilename is the <code>String</code> with the name of the file.
+     */
+    public SoundClip(String sFilename) {
+        // Calls the default contructor.
+        this();
+
+        // Loads the sound file.
+        load(sFilename);
+    }
+
+    /**
+     * Access method that returns a type Clip object.
+     *
+     * @return clip is a <code>Clip object</code>.
+     */
+    public Clip getClip() {
+        return cClip;
+    }
+
+    /**
+     * Modifier method used to modify if the sound will be repeated.
+     *
+     * @param bLooping is a <code>boolean</code> value.
+     */
+    public void setLooping(boolean bLooping) {
+        this.bLooping = bLooping;
+    }
+
+    /**
+     * Access method that returns a boolean to know if the sound is repeated.
+     *
+     * @return bLooping is a <code>boolean</code> value.
+     */
+    public boolean getLooping() {
+        return bLooping;
+    }
+
+    /**
+     * Modifier method used to define the number of repetitions.
+     *
+     * @param iRepeat is an <code>integer</code> that represents the number of
+     * repetitions.
+     */
+    public void setRepeat(int iRepeat) {
+        this.iRepeat = iRepeat;
+    }
+
+    /**
+     * Access method that returns the number of repetitions.
+     *
+     * @return iRepeat is an <code>integer</code> value that represents the
+     * number of repetitions.
+     */
+    public int getRepeat() {
+        return iRepeat;
+    }
+
+    /**
+     * Modifier method that assigns the name of the file.
+     *
+     * @param sFilename is a <code>String</code> with file name.
+     */
+    public void setFilename(String sFilename) {
+        this.sFilename = sFilename;
+    }
+
+    /**
+     * Access method that returns the name of the file.
+     *
+     * @return sFilename is a <code>String</code> with file name.
+     */
+    public String getFilename() {
+        return sFilename;
+    }
+
+    /**
+     * Method that verifies if the audio file is loaded.
+     *
+     * @return aiSample is a<code>sample object</code>.
+     */
+    public boolean isLoaded() {
+        return (boolean) (aiSample != null);
+    }
+
+    /**
+     * Access method that returns the URL of the file.
+     *
+     * @param sFilename is a <code>String</code> with the file name.
+     */
+    private URL getURL(String sFilename) {
+        URL url = null;
+
+        try {
+            url = this.getClass().getResource(sFilename);
+        } catch (Exception exc) {
         }
+
+        return url;
+    }
+
+    /**
+     * Method that loads the audio file.
+     *
+     * @param sAudiofile is a <code>String</code> with the file name of the
+     * audio.
+     *
+     * @return a <code>boolean</code> value.
+     */
+    public boolean load(String sAudiofile) {
+        try {
+            setFilename(sAudiofile);
+            aiSample = AudioSystem.getAudioInputStream(getURL(sFilename));
+            cClip.open(aiSample);
+            return true;
+
+        } catch (IOException e) {
+            return false;
+        } catch (UnsupportedAudioFileException e) {
+            return false;
+        } catch (LineUnavailableException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Method that reproduces the sound.
+     */
+    public void play() {
+        // Return with no actions if the sound is not loaded.
+        if (!isLoaded()) {
+            return;
+        }
+        // Starts playing from the start of the file.
+        cClip.setFramePosition(0);
+
+        // Play with the looping option.
+        if (bLooping) {
+            cClip.loop(Clip.LOOP_CONTINUOUSLY);
+        } else {
+            cClip.loop(iRepeat);
+        }
+    }
+
+    /**
+     * Method that stops the audio.
+     */
+    public void stop() {
+        cClip.stop();
+        // Set the clip time in 0, so it starts again if needed.
+        lClipTime = 0;
+    }
+
+    /**
+     * Method that pauses the audio.
+     */
+    public void pause() {
+        // Get the actual position and save it for later.
+        lClipTime = cClip.getMicrosecondPosition();
+        cClip.stop();
+
+    }
+
+    /**
+     * Method that unpauses the audio.
+     */
+    public void unpause() {
+        // Play from where it left the last time.
+        cClip.setMicrosecondPosition(lClipTime);
+
+        // Play with the looping option.
+        if (bLooping) {
+            cClip.loop(Clip.LOOP_CONTINUOUSLY);
+        } else {
+            cClip.loop(iRepeat);
+        }
+    }
 }
